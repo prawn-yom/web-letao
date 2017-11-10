3bc0e4a9409dcc07f15fe2ea5ed89b14942d2051
prawn-yom <377617721@qq.com>
2017-11-10 20:24:42 +0800

    完成了首页的侧边栏静态

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -6,6 +6,6 @@ $(document).ajaxStart(function () {
 $(document).ajaxStop(function () {
     // 为了凸显进度条的消失 在此定了一个计时器 （只是为了本地体验效果）
     setTimeout(function() {
-        NProgress.done();      
+        NProgress.done();
     }, 500);
 });
