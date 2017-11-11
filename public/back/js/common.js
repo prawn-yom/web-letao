71fc6eac7ac288c13027a8f9027390cf99b4a210
prawn-yom <377617721@qq.com>
2017-11-11 21:37:39 +0800

    首页全部完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,8 +1,11 @@
+/* 进度条 */
+// 进度条右边的圆圈
 NProgress.configure({ showSpinner: false });
+// 进度条在ajax开始的时候 触发
 $(document).ajaxStart(function () {
     NProgress.start();
 });
-
+// 进度条在ajax结束的时候 结束
 $(document).ajaxStop(function () {
     // 为了凸显进度条的消失 在此定了一个计时器 （只是为了本地体验效果）
     setTimeout(function() {
